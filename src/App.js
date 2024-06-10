38728eeb65d9191e9483dc6f291556588f4f1c0e
wanjohi lewis <wanjohilewi7@gmail.com>
2024-06-10 23:28:43 +0300

    youtube clone using react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './componets/Navbar';
+
+const App = () => (
+    
+      <BrowserRouter>
+        <Box sx={{ backgroundColor: '#ooo' }}>
+          <Navbar />
+          <Routes>
+            <Route path="/" exact element={<Feed />}/>
+            <Route path="/video/:id" exact element={<VideoDetail />}/>
+            <Route path="/channel/:id" exact element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm" exact element={<SearchFeed />}/>
+          </Routes>
+        </Box>
+      </BrowserRouter>
+);
+
+export default App
